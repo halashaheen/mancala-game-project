1791bf5eca37c69fb894efff8b7d7084893a3b5c
halashaheen <hala1731998@gmail.com>
2021-06-11 20:47:39 +0200

    adding AImove and Main function

--- mancala.py
+++ mancala.py
@@ -1,9 +1,20 @@
 import math
 import copy
+import time
 import pickle
+import emoji
+import os
+
+from pyfiglet import Figlet
+from termcolor import colored
 
 #player value
 opponent, AI =  False, True
+#mode selection values
+mode_dict = {0:"withoutStealMode",1:"withStealMode"}
+#file name
+fileName = ""
+
 
 def printMancala(mancala_board):
     """
@@ -49,6 +60,17 @@ def isMancalaIndex(player,lastIndex):
     else:
         print("error from isMancalaIndex !!!!!!!!")
         return
+        
+def belongToPlayer(player,pocket_index):
+    if(player == AI):
+        if(pocket_index<6):return True
+        else:              return False
+    elif(player == opponent):
+        if(pocket_index>=7):return True
+        else:               return False
+    else:
+        print("error from belongToPlayer !!!!")
+        return
 
 """
 checks for the condition of stealing : the laststone is placed in an empty pocket belonging to the same player 
@@ -57,7 +79,6 @@ parameters: 1- board:mancala board
             2- lastIndex:last move performed by player
             3- player:opponent or AI
 returns: true if condition of stealing is met otherwise false
-
 """
 def isSteal(board,lastIndex,player):
     oppositeIndex = 12 -lastIndex
@@ -169,7 +190,7 @@ def updateBoard(board,pocket_index,mode):
             if(not(playAgain)):
                 #we check that last stone might be placed in an empty pocket of the opponent
                 #opponent should steal
-                if(isSteal(board,lastIndex,pocket_index,opponent)):#go prevBoard
+                if(isSteal(board,lastIndex,opponent)):#go prevBoard
                     updateBoardWithSteal(board,pocket_index)
                     #even if last stone is placed in same pocket played
                     #opponent will steal
@@ -196,7 +217,7 @@ def updateBoard(board,pocket_index,mode):
             if(not(playAgain)):
                 #we check that last stone might be placed in an empty pocket of the AI
                 #AI should steal
-                if(isSteal(board,lastIndex,pocket_index,AI)):
+                if(isSteal(board,lastIndex,AI)):
                     updateBoardWithSteal(board,pocket_index)
                     #even if last stone is placed in same pocket played
                     #AI will steal
@@ -240,7 +261,6 @@ check whether the pocket is empty or not
 parameters: 1- board:mancala board
             2- pocket_index : the index of the pocket
 returns: true if the pocket at that index is empty or not 
-
 """
 def isPocketEmpty(board,pocket_index):
     if(board[pocket_index] ==0):
@@ -252,7 +272,6 @@ check whether the all mancala pocket are empty or not
 parameters: 1- board:mancala board
             2- player:opponent or AI
 returns: true if the all pockets are empty or not 
-
 """
 def AllPocketsEmpty(board,player):
     if(player == AI):
@@ -278,7 +297,6 @@ returns whether stop condition is true or false
 parameters: 1- board:mancala board
             2- player:opponent or AI
 returns: true if the stop played condition is met otherwise false
-
 """
 def stopPlay(board,player):
     #AI pockets are all empty
@@ -311,7 +329,6 @@ parameters: 1- board:mancala board
             2- mode:with/without stealing
             3- endOfGame:boolean represents whether the game ends or not
 returns: the updated board after opponent move
-
 """
 
 def oppMove(board,mode,endOfGame):
@@ -396,7 +413,7 @@ def minimax(board,mode,depth,depthMax,player,alpha,beta):
     #check the result of game
     if(StopPlayAI or StopPlayOpp or depthLimit):
         # return evaluateWrtDepth(board,player,depth)  #hard level evaluation function
-        return evaluateWrtDepth(board,player)
+        return evaluate(board)
     else:
         #AI turn
         if(player == AI):
@@ -476,8 +493,165 @@ def minimax(board,mode,depth,depthMax,player,alpha,beta):
         else:
             print("error in minimax !!!!!")
             return
+    
 
+"""
+
+Check if condition of end of game is met or not
+parameters:mancala board
+returns: True if all pockets of both players are empty,false otherwise
+
+"""
+
+def endGame(board):
+    for i in range(14):
+        if(i!=6 and i!=13):
+            if(board[i]!=0): return False
+    return True
+
+"""
+
+check for condition of winning and display a message showing who is the winner
+parameters: board
+returns: none
+
+"""
+def printWinner(board):
+    if(board[6]>board[13]):
+        Loser()
+        print(bcolors.red+"GAME OVER, YOU LOSE "+ bcolors.RESET,end="")
+        print("\U0001F923\U0001F923\U0001F923")
+    elif(board[6]<board[13]):
+        winner()
+        print(bcolors.green+"WINNER "+ bcolors.RESET,end="")
+        print(emoji.emojize(":party_popper:"),end="")
+        print(emoji.emojize(":party_popper:"))
+    else:
+        print(bcolors.yellow+"DRAW !!!!!"+ bcolors.RESET)
+        
+        
+"""
+checks if any of two players steal or not and display the message showing which player is stealing
+parameters: prevBoard:board before move 
+            currBoard:board afer move 
+            pocket_index:index of the played pocket causing the move 
+returns:none
+"""
+
+def printSteal(prevBoard,currBoard,pocket_index):
+    #initialize counter of insertion in mancala
+    N_mancala = 0
+    addedStonesDueToStealByAI = 0
+    addedStonesDueToStealByOpponent = 0
+    #calculate number of times in insert in mancala
+    if(pocket_index<6):
+        if(prevBoard[pocket_index] >=6-pocket_index):
+            N_mancala = math.floor((6-pocket_index +prevBoard[pocket_index])/14)+1
+        addedStonesDueToStealByAI = currBoard[6]-N_mancala-prevBoard[6]
+        #AI steals
+        if(addedStonesDueToStealByAI>0):
+            print(bcolors.BOLD+bcolors.purple+"AI is stealing "+bcolors.RESET,end="")
+            print(emoji.emojize(":fire:"))
+        #no stealing
+        else:
+            return
+
+    elif(pocket_index>=7):
+        if(prevBoard[pocket_index] >=13-pocket_index):
+            N_mancala = math.floor((13-pocket_index +prevBoard[pocket_index])/14)+1
+        addedStonesDueToStealByOpponent = currBoard[13]-N_mancala-prevBoard[13]
+        #opponent steals
+        if(addedStonesDueToStealByOpponent>0):
+            print(bcolors.BOLD+bcolors.purple+"Opponent is stealing "+bcolors.RESET,end="")
+            print(emoji.emojize(":fire:"))
+        #no stealing
+        else:
+            return
+  
+    else:
+        print("error from printSteal !!!!")
+        return
+
+"""
+
+performs AI move based on decision evaluated by minimax algorithm and evaluation/utility function
+parameters: board:mancala board
+	    mode:stealing or without stealing
+	    depthMax:maximum depth of AI tree
+	    printBoard:boolean represents when the board needs to be printed to avoid redundant prints
+returns:best score and updated board
+ 
+"""
+def AIMove(board,mode,depthMax,printBoard):
+    startTime = time.time()
+    board,StopPlayAI = stopPlay(board,AI)
+    # board,StopPlayOpponent = stopPlay(board,opponent)
+    bestScore = -1000
+    bestMove = -1
+    if(not(StopPlayAI)):
+        #saving our board before any moves
+        prevBoard = copy.deepcopy(board)
+        #loop over AI possible moves
+        for i in range(6):
+            if(not(isPocketEmpty(board,i))):
+                #move
+                playAgain = updateBoard(board,i,mode)
+                if(playAgain):
+                    score,board= AIMove(board,mode,depthMax,False)                 
+                    #undo
+                    board=copy.deepcopy(prevBoard)
+                else:
+                    score = minimax(board,mode,0,depthMax,opponent,-1000,1000)
+                    #undo
+                    board=copy.deepcopy(prevBoard)
+                # evaluate best move
+                if (score >= bestScore):
+                    bestScore = score
+                    bestMove = i
+
+        #AI plays best move
+        boardBeforeUpdate = copy.deepcopy(board)
+        again = updateBoard(board,bestMove,mode)
+
+        if(printBoard):
+            print(bcolors.yellow+"********************************AI turn*****************************************")
+            print(bcolors.yellow+"AI played pocket number= ",bestMove)
+            if(mode_dict[mode]=="withStealMode"):
+                printSteal(boardBeforeUpdate,board,bestMove)
+            print(bcolors.RESET,end="")
+            print()
+            printMancala(board)
+            endTime = time.time()
+            print("time taken by AI to make the move : ",endTime-startTime,"sec")
+
+            if(endGame(board)):
+                print(bcolors.purple+"*****************************END OF GAME****************************************"+bcolors.RESET)
+                printMancala(board)
+                printWinner(board)
+                return bestScore,board
+        if(again):
+            score,board = AIMove(board,mode,depthMax,printBoard)
+        board,StopPlayAI = stopPlay(board,AI)
+        #prevent printing when game ends
+        end = endGame(board)
+        if(StopPlayAI  and not(end)):
+            print(bcolors.purple+"*****************************END OF GAME****************************************"+bcolors.RESET)
+            printMancala(board)
+            printWinner(board)
+            return bestScore,board
+        else:
+            return bestScore,board
+    else:
+        if(printBoard):
+            print(bcolors.purple+"*****************************END OF GAME****************************************"+bcolors.RESET)
+            printMancala(board)
+            printWinner(board)
+        return bestScore,board
+        
+        
 ####################################################### BONUS 1 ############################################################
+if not os.path.exists("games"):
+        os.mkdir("games")
 
 def saveGame(fileName,board,player,mode,level):
     """
@@ -500,3 +674,130 @@ def loadGame(fileName):
     buffer = open (fileName, "rb")
     game = pickle.load(buffer)
     return game
+    
+
+####################################################### BONUS 2 ############################################################
+
+"""
+difficulty levels : is all about varying depth to make AI make better movement 
+level: Easy - Meduim -hard
+ 
+"""
+def easyLevel(board,mode,printBoard):
+    return AIMove(board,mode,2,printBoard)
+
+def meduimLevel(board,mode,printBoard):
+    return AIMove(board,mode,4,printBoard)
+    
+def hardLevel(board,mode,printBoard):
+    return AIMove(board,mode,6,printBoard)
+ 
+#color class to make terminal colorful :)   
+class bcolors:
+    BOLD = '\033[1m' #BOLD
+    green = '\033[92m' #GREEN
+    red = '\033[91m' #RED
+    yellow = '\033[93m' #YELLOW
+    blue = '\033[94m' #blue
+    purple = '\033[95m' #purple
+    RESET = '\033[0m' #RESET COLOR
+
+#main function to run game
+if __name__ == "__main__":
+
+    #taking input from user
+    f = Figlet(font='bulbhead')
+    print(colored(f.renderText('Game Starts'),'yellow'))
+
+    print("\033[1;40;94m Do you want to load a previous game ? \033[0m\n\n - 0 : NO\n - 1 : YES\n")
+    isLoad =input("Load game or not  : ")
+    print()
+    #modifying isLoad to be int
+    isLoad = int(isLoad)
+    if(isLoad):
+	    print("\033[1;40;94m please enter the file name to be loaded : \033[0m",end="")
+	    fileLoad =input("")
+	    game = loadGame(fileLoad)
+	    mancala_board = game[0]
+	    player = not(game[1])
+	    mode = int(game[2])
+	    level = int(game[3])
+	    print()
+
+    print("\033[1;40;94m Do you want to save this Game ? \033[0m\n\n - 0 : not Save\n - 1 : Save\n")
+    isSave =input("save game or not  : ")
+    print()
+    #modifying isSave to be int
+    isSave = int(isSave)
+    if(isSave):
+	    print("\033[1;40;94m please enter the file name  : \033[0m",end="")
+	    fileSave =input("")
+	    print()
+    if(not(isLoad)):
+	    print("\033[1;40;94m Please Enter required Mode of Game \033[0m\n\n - 0 : for WithoutSteal\n - 1 : for WithSteal\n")
+	    mode =input("Mode of Game : ")
+	    print()
+	    print("\033[1;40;94m Please Enter who should start the Game \033[0m\n\n - 0 : for HUMAN\n - 1 : for AI\n")
+	    player =input("initial player  : ")
+	    print()
+
+	    print("\033[1;40;94m Please Enter the level of Game \033[0m\n\n - 1 : for Easy\n - 2 : for Meduim\n - 3 : for Hard \n")
+	    level =input("level  : ")
+	    print()
+
+	    #modifying level to be int
+	    level = int(level)
+	    #modifying mode to be int
+	    mode = int(mode)
+	    #modifying palyer value to match call in functions
+	    if(int(player) ==0):
+	    	player = opponent
+	    elif(int(player) ==1):
+	    	player =AI
+	    else:
+	    	print(bcolors.red+ "error in player !!!!!" + bcolors.RESET)
+
+	    #initial state of mancala
+	    mancala_board = [4,4,4,4,4,4,0,4,4,4,4,4,4,0]
+    #visualization of mancala
+    printMancala(mancala_board)
+
+    #check condition of stop game
+    _,check = stopPlay(mancala_board,player)
+    while(not check):
+        if(player):
+            # start = time.time()
+            if(mode_dict[mode] == "withoutStealMode" or mode_dict[mode] == "withStealMode"):
+                if(level ==1):
+                    #easy level
+                    _,mancala_board = easyLevel(mancala_board,mode,True)
+                elif(level ==2):
+                    #meduim level
+                    _,mancala_board = meduimLevel(mancala_board,mode,True)
+                elif(level ==3):
+                    #hard level
+                    _,mancala_board = hardLevel(mancala_board,mode,True)
+                else:
+                    print("error in level !!!!")
+                    break
+            else:
+                print("error in mode !!!!!!")
+            if(isSave):
+                    # print("saving AI game ....")
+                    saveGame(fileSave,mancala_board,player,mode,level)
+            _,check = stopPlay(mancala_board,player)
+            player = opponent
+            # end = time.time()
+            # print("time taken by AI to make the move : ",end-start,"sec")
+        else:
+            if(mode_dict[mode] == "withoutStealMode"or mode_dict[mode] == "withStealMode"):
+                oppMove(mancala_board,mode,False)
+                if(isSave):
+                    # print("saving opponent game ....")
+                    saveGame(fileSave,mancala_board,player,mode,level)
+            else:
+                print("error in mode !!!!!!")
+            _,check = stopPlay(mancala_board,player)
+            player = AI
+k=input("press Enter to exit") 
+       
